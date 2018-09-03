87f4d25674896bd468ec945706b22308bf014b61
Willem Houm <willem.houm@gmail.com>
2018-09-03 11:54:12 +0200

    init

--- server/app.js
+++ server/app.js
@@ -10,6 +10,9 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+var users = [];
+
+
 app.use(express.static(path.join(__dirname, '/..', 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +22,23 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !', socket);
+    console.log('Un client est connecté !');
+    socket.on('username', function (message) {
+        console.log(message);
+        socket.username = message.username;
+        users.push(socket.username);
+        socket.send('Your username is ' + socket.username);
+    });
+    socket.on('message', function(message) {    
+        console.log(message);
+        
+    });
 });
+
+
+var interval = setInterval(function () {
+    io.emit('userlist', users)
+}, 3000)
 
 server.listen(process.env.APP_PORT, function () {
     console.log('Server listening on port : ' + process.env.APP_PORT);
