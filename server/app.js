e78dac940b5c7087406ad6d43e6f816b44c1baaf
Willem Houm <willem.houm@gmail.com>
2018-09-04 12:00:55 +0200

    dissociated socket server

--- server/app.js
+++ server/app.js
@@ -7,11 +7,6 @@ var path = require('path'),
     express = require('express'),
     http = require('http');
 var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-
-var users = [];
-
 
 app.use(express.static(path.join(__dirname, '/..', 'public')));
 app.use(bodyParser.json());
@@ -26,19 +21,6 @@ app.get('/', function (req, res) {
     res.sendFile('../public/index.html', { root: __dirname });
 });
 
-io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !');
-    socket.on('username', function (message) {
-        socket.username = message.username;
-        users.push({username: socket.username});
-        _sendUserList(users);
-    })
-});
-
-server.listen(process.env.APP_PORT, function () {
+app.listen(process.env.APP_PORT, function () {
     console.log('Server listening on port : ' + process.env.APP_PORT);
 });
-
-function _sendUserList(array) {
-    io.emit('userlist', array);
-}
